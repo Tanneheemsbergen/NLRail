d96bc466f7fa5316d01ae0cfa9f38a3bbb3f9004
Charlotte Hoffmans <charlotte@LAPTOP-PPUPCLSV.localdomain>
2022-06-08 14:22:49 +0200

    added total time

--- random_alg.py
+++ random_alg.py
@@ -13,35 +13,36 @@ for line in connecties[1:]:
     all_connections.append(connection)
 
 trajects = []
-
+total_time_traject = 0
 for i in range(MAX_AMOUNT_TRAJECTS):
+    copy_connecties = all_connections.copy()
     traject = []
     total_time = 0
     station = random.choices(all_connections, k=1)[0][0]
     traject.append(station)
 
     while  total_time < MAX_TIME:
-        print("station: ")
-        print(station)
+        # print("station: ")
+        # print(station)
         options_next_station = []
         for i in all_connections:
             if i[0] == station:
                 options_next_station.append(i)
         if len(options_next_station) == 0:
             break
-        print("options: ")
-        print(options_next_station)
+        # print("options: ")    
+        # print(options_next_station)
         next_station = random.choices(options_next_station, k=1)[0]
-        print("option next station: ")
-        print(next_station)
-        all_connections.remove(next_station)
-
+        # print("option next station: ")
+        # print(next_station)
+        copy_connecties.remove(next_station)
+        
         traject.append(next_station[1])
         total_time += int(i[2])
         station = next_station[1]
     trajects.append(traject)
+    total_time_traject += total_time
 
-    print(traject)
-    print(total_time)
 
 print(trajects)
+print(total_time_traject)
