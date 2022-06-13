471d2ce4cdb37eb3d651723f1e6153fa3e2ec5ed
Debby Bouma <debbybouma@hotmail.com>
2022-06-13 15:11:21 +0200

    Main update

--- main.py
+++ main.py
@@ -1,29 +1,53 @@
 from functions.heuristics.random_alg import random_function
-from functions.visualisation import visualisation
-with open("csvfiles/ConnectiesHolland.csv") as f:
-    connecties = f.readlines()
-all_connections = []
+from classes.graph_class import Graph
+import argparse
+#from functions.visualisation import visualisation
 
-for line in connecties[1:]:
-    connection = []
-    for i in line.strip().split(','):
-        connection.append(i)
-    all_connections.append(connection)
-MAX_AMOUNT_TRAJECTS = 7
-MAX_TIME = 120
-random_function(all_connections, MAX_AMOUNT_TRAJECTS, MAX_TIME)
-visualisation(traject, all_stations)
+def main (input_file_name, algorithm):  
+    # Open de input document and create an empty list for connections
+    with open(f"csvfiles/{input_file_name}.csv") as f:
+        connecties = f.readlines()
+    all_connections = []
 
-# if __name__ == "__main__":
-#     # Set-up parsing command line arguments
-#     parser = argparse.ArgumentParser(description = "run algorithm")
-#
-#     # Adding arguments
-#     parser.add_argument("area", type=int, choices=[1, 2], help="1: North/South-Holand, 2: Holland")
-#     parser.add_argument("heuristics", type=int, choices=[1, 2],, help="1: random, 2: greedy")
-#
-#     # Read arguments from command line
-#     args = parser.parse_args()
-#
-#     # Run main with provide arguments
-#
+    # Fill the connections list with all connections
+    for line in connecties[1:]:
+        connection = []
+        for i in line.strip().split(','):
+            connection.append(i)
+        all_connections.append(connection)
+
+    # Determine the maximum amount of trajects and time, based on the user input
+    if input_file_name == "ConnectiesHolland":
+        MAX_AMOUNT_TRAJECTS = 7
+        MAX_TIME = 120
+    elif input_file_name == "ConnectiesNationaal":
+        MAX_AMOUNT_TRAJECTS = 20
+        MAX_TIME = 180
+
+    # Determine which algorithm to use based on the user input
+    if algorithm == "r":
+        random_function(all_connections, MAX_AMOUNT_TRAJECTS, MAX_TIME)
+    # elif algorithm = "g":
+    #     greedy_function(all_connections, MAX_AMOUNT_TRAJECTS, MAX_TIME)
+    # elif algorithm = "":
+
+    # Visualization
+    #Graph("csvfiles/ConnectiesHolland.csv")
+    #visualisation(traject, all_stations)
+    from classes.graph_class import Graph
+    test_graph = Graph("csvfiles/StationsHolland.csv")
+    print(random_function_classes(test_graph, 7, 120))
+
+if __name__ == "__main__":
+    # Set-up parsing command line arguments
+    parser = argparse.ArgumentParser(description = "run algorithm")
+
+    # Adding arguments
+    parser.add_argument("Area", choices=["ConnectiesHolland", "ConnectiesNationaal"], help="1: Connections in North - and South-Holland, 2: The entire Netherlands")
+    parser.add_argument("Heuristics", choices=["r", "g"], help="1: Random, 2: Greedy")
+
+    # Read arguments from command line
+    args = parser.parse_args()
+
+    # Run main with provide arguments
+    main(args.Area, args.Heuristics)
