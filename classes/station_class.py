01d77dc57b86729b9d5b736c2cad6da11c64050d
Charlotte Hoffmans <charlotte@LAPTOP-PPUPCLSV.localdomain>
2022-06-13 15:01:13 +0200

    pulll

--- classes/station_class.py
+++ classes/station_class.py
@@ -6,36 +6,20 @@ class Station(object):
         self.name = name
         self.xcoordinate = xcoordinate
         self.ycoordinate = ycoordinate
-        self.connections = self.load_connection()
         self.time = self.load_connection_time()
 
-
-    def load_connection(self):
-        connections = []
-        with open("csvfiles/ConnectiesHolland.csv", 'r') as in_file:
-            reader = csv.DictReader(in_file)
-             
-            for row in reader:
-                if row['station1'] == self.name:
-                    connections.append(row['station2'])
-
-                elif row['station2'] == self.name:
-                    connections.append(row['station1'])
-
-        return connections
     def load_connection_time(self):
-        connections = []
         connection_time = {}
         with open("csvfiles/ConnectiesHolland.csv", 'r') as in_file:
             reader = csv.DictReader(in_file)
-             
+          
             for row in reader:
                 if row['station1'] == self.name:
     
                     connection_time[row['station2']] = row['distance']
                 elif row['station2'] == self.name:
                     connection_time[row['station1']] = row['distance']
-            # connections.append(connection_time)
+
         return connection_time
 
 
@@ -44,8 +28,8 @@ class Station(object):
         Make sure that the object is printed properly if it is in a list/dict.
         """
         connections = "["
-        for connection in self.connections:
-            if connection == self.connections[-1]:
+        for connection in list(self.time.keys()):
+            if connection == list(self.time.keys())[-1]:
                 connections += (f"{connection}")
             else:
                 connections += (f"{connection}, ")
