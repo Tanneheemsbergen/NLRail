409c04b596e3f976c824ee2081e4015a398b0c0b
Debby Bouma <debbybouma@hotmail.com>
2022-06-13 12:14:50 +0200

    Random alg fix

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from functions.heuristics.random_alg import random_function
-#from functions.visualisation import visualisation
+from functions.visualisation import visualisation
 with open("csvfiles/ConnectiesHolland.csv") as f:
     connecties = f.readlines()
 all_connections = []
@@ -9,18 +9,17 @@ for line in connecties[1:]:
     for i in line.strip().split(','):
         connection.append(i)
     all_connections.append(connection)
-
 MAX_AMOUNT_TRAJECTS = 7
 MAX_TIME = 120
 random_function(all_connections, MAX_AMOUNT_TRAJECTS, MAX_TIME)
-#visualisation(traject, all_stations)
+visualisation(traject, all_stations)
 
 # if __name__ == "__main__":
 #     # Set-up parsing command line arguments
 #     parser = argparse.ArgumentParser(description = "run algorithm")
 #
 #     # Adding arguments
-#     parser.add_argument("area", type=int, choices=[1, 2], help="1: North/South-Holland, 2: Holland")
+#     parser.add_argument("area", type=int, choices=[1, 2], help="1: North/South-Holand, 2: Holland")
 #     parser.add_argument("heuristics", type=int, choices=[1, 2],, help="1: random, 2: greedy")
 #
 #     # Read arguments from command line
