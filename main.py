0586ab4e2171732980739d0788faf0b99e7d6b67
Debby Bouma <debbybouma@hotmail.com>
2022-06-13 15:18:04 +0200

    Main update

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from functions.heuristics.random_alg import random_function
-from classes.graph_class import Graph
+#from classes.graph_class import Graph
 import argparse
 #from functions.visualisation import visualisation
 
 def main (input_file_name, algorithm):  
     # Open de input document and create an empty list for connections
-    with open(f"csvfiles/{input_file_name}.csv") as f:
+    input_file = f"csvfiles/{input_file_name}.csv"
+    with open(input_file) as f:
         connecties = f.readlines()
     all_connections = []
 
@@ -27,16 +28,16 @@ def main (input_file_name, algorithm):
     # Determine which algorithm to use based on the user input
     if algorithm == "r":
         random_function(all_connections, MAX_AMOUNT_TRAJECTS, MAX_TIME)
-    elif algorithm = "g":
+    elif algorithm == "g":
         greedy_function(all_connections, MAX_AMOUNT_TRAJECTS, MAX_TIME)
     # elif algorithm = "":
 
     # Visualization
     #Graph("csvfiles/ConnectiesHolland.csv")
     #visualisation(traject, all_stations)
-    from classes.graph_class import Graph
-    test_graph = Graph("csvfiles/StationsHolland.csv")
-    print(random_function_classes(test_graph, 7, 120))
+    # from classes.graph_class import Graph
+    # test_graph = Graph("csvfiles/StationsHolland.csv")
+    # print(random_function_classes(test_graph, 7, 120))
 
 if __name__ == "__main__":
     # Set-up parsing command line arguments
